bf96642776ba810866f194790b8418feb1323d83
Vishwanath Ezhil <54872169+vishwa742@users.noreply.github.com>
2021-05-20 01:12:54 -0400

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -12,9 +12,9 @@ export default () => {
     <>
       <Helmet>
         <meta charSet="utf-8" />
-        <title>{title || 'Gatsby Simplefolio'}</title>
+        <title>{title || 'Vishwanath Ezhil'}</title>
         <html lang={lang || 'en'} />
-        <meta name="description" content={description || 'Gatsby Simplefolio'} />
+        <meta name="description" content={description || 'Vishwanath Ezhil'} />
       </Helmet>
       <App />
     </>
